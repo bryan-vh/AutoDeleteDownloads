ee70f655bb0c33359b51c5745b26187ed9d1d55d
Bryan Van Horn <bvhwa@yahoo.com>
2019-11-21 17:25:14 -0800

    Initial commit

---
+++ AutoDeleteDownloads.py
@@ -0,0 +1,50 @@
+import os
+from datetime import datetime
+
+
+def remove_download_files(downloads, now, retain):
+    # Go through my downloads and attempt to delete items that are over a certain # of days old
+    for file in os.listdir(downloads):
+        path = downloads + file
+
+        if os.path.isfile(path) and not file.startswith(".", 0, 1):
+            stat = os.stat(path)
+
+            # Can only get birth time on Mac (convenient for me)
+            try:
+                birth_time_ts = stat.st_birthtime
+                birth_time = datetime.fromtimestamp(birth_time_ts)
+
+                diff = now - birth_time
+
+                if diff.days > retain:
+                    while True:
+                        will_delete = input("Do you want to delete: {file}? ".format(file=file))
+                        will_delete.lower()
+
+                        if will_delete == "yes" or will_delete == "y":
+                            os.remove(path)
+                            break
+                        elif will_delete == "no" or will_delete == "n":
+                            break
+                        else:
+                            continue
+            except AttributeError:
+                print("Oops!")
+
+
+def main():
+    # Get the downloads folder for the user
+    downloads_folder = os.path.expanduser("~/Downloads/")
+
+    # How many days far back should we retain downloads (change to your liking)
+    retain = int(input("How far back (in days) do you want to not delete files? "))
+
+    # Get current time
+    now = datetime.now()
+
+    remove_download_files(downloads_folder, now, retain)
+
+
+if __name__ == "__main__":
+    main()
