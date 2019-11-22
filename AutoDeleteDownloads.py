a72e3c6e752b60da46f8309f3336007c8247071e
Bryan Van Horn <bvhwa@yahoo.com>
2019-11-21 19:51:29 -0800

    Added additional spacing between statements and added a summary statement.

--- AutoDeleteDownloads.py
+++ AutoDeleteDownloads.py
@@ -3,6 +3,11 @@ from datetime import datetime
 
 
 def remove_download_files(downloads, now, retain):
+    removed = 0
+    total = 0
+
+    print()
+
     # Go through my downloads and attempt to delete items that are over a certain # of days old
     for file in os.listdir(downloads):
         path = downloads + file
@@ -18,12 +23,15 @@ def remove_download_files(downloads, now, retain):
                 diff = now - birth_time
 
                 if diff.days > retain:
+                    total += 1
+
                     while True:
                         will_delete = input("Do you want to delete: {file}? ".format(file=file))
                         will_delete.lower()
 
                         if will_delete == "yes" or will_delete == "y":
                             os.remove(path)
+                            removed += 1
                             break
                         elif will_delete == "no" or will_delete == "n":
                             break
@@ -31,6 +39,10 @@ def remove_download_files(downloads, now, retain):
                             continue
             except AttributeError:
                 print("Oops!")
+
+    print("\n"
+          "Removed {removed} files out of {total} eligible files."
+          "\n".format(removed=removed, total=total))
 
 
 def main():
